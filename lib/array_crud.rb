568c2d107b42d89d9b01f89f916aa4250b861392
Paul Beckwith <303dog@gmail.com>
2019-07-21 18:55:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ def white = create_an_array.pop
   
 end
 
-def remove_element_from_start_of_array(array)
+def red = create_an_array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  create_an_array [1]
 end
 
 def retrieve_first_element_from_array(array)
